bc22f328a31b5fee0bce866dc94302e1350e3560
Yu Chen <intellectual.y@gmail.com>
2023-05-12 11:05:58 +0800

    reactjs toggle display of a sample form (cake profile edit)

--- components/SharedModal.tsx
+++ components/SharedModal.tsx
@@ -4,19 +4,18 @@ import {
   ArrowUturnLeftIcon,
   ChevronLeftIcon,
   ChevronRightIcon,
+  PencilSquareIcon,
   XMarkIcon,
-} from '@heroicons/react/24/outline'
-import { AnimatePresence, motion, MotionConfig } from 'framer-motion'
-import Image from 'next/image'
-import { useState } from 'react'
-import { useSwipeable } from 'react-swipeable'
-import { variants } from '../utils/animationVariants'
-import downloadPhoto from '../utils/downloadPhoto'
-import { range } from '../utils/range'
-import type { ImageProps, SharedModalProps } from '../utils/types'
-import Twitter from './Icons/Twitter'
-
-
+} from "@heroicons/react/24/outline";
+import { AnimatePresence, motion, MotionConfig } from "framer-motion";
+import Image from "next/image";
+import { useState } from "react";
+import { useSwipeable } from "react-swipeable";
+import { variants } from "../utils/animationVariants";
+import downloadPhoto from "../utils/downloadPhoto";
+import { range } from "../utils/range";
+import type { ImageProps, SharedModalProps } from "../utils/types";
+import Twitter from "./Icons/Twitter";
 
 export default function SharedModal({
   index,
@@ -27,33 +26,37 @@ export default function SharedModal({
   currentPhoto,
   direction,
 }: SharedModalProps) {
-  const [loaded, setLoaded] = useState(false)
+  const [loaded, setLoaded] = useState(false);
+  const [showForm, setShowForm] = useState(false);
+
+  function toggleShowForm() {
+    setShowForm(!showForm);
+  }
 
   let filteredImages = images?.filter((img: ImageProps) =>
     range(index - 15, index + 15).includes(img.id)
-  )
+  );
 
   const handlers = useSwipeable({
     onSwipedLeft: () => {
       if (index < images?.length - 1) {
-        changePhotoId(index + 1)
+        changePhotoId(index + 1);
       }
     },
     onSwipedRight: () => {
       if (index > 0) {
-        changePhotoId(index - 1)
+        changePhotoId(index - 1);
       }
     },
     trackMouse: true,
-  })
+  });
 
-  let currentImage = images ? images[index] : currentPhoto
-
+  let currentImage = images ? images[index] : currentPhoto;
 
   return (
     <MotionConfig
       transition={{
-        x: { type: 'spring', stiffness: 300, damping: 30 },
+        x: { type: "spring", stiffness: 300, damping: 30 },
         opacity: { duration: 0.2 },
       }}
     >
@@ -74,18 +77,76 @@ export default function SharedModal({
                 exit="exit"
                 className="absolute"
               >
-                <Image
-                  src={`https://res.cloudinary.com/${
-                    process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME
-                  }/image/upload/c_scale,${navigation ? 'w_1280' : 'w_1920'}/${
-                    currentImage.public_id
-                  }.${currentImage.format}`}
-                  width={navigation ? 1280 : 1920}
-                  height={navigation ? 853 : 1280}
-                  priority
-                  alt="Next.js Conf image"
-                  onLoadingComplete={() => setLoaded(true)}
-                  />
+                <div>
+                  <div style={showForm ? { display: "none" } : {}}>
+                    <Image
+                      src={`https://res.cloudinary.com/${
+                        process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME
+                      }/image/upload/c_scale,${
+                        navigation ? "w_1280" : "w_1920"
+                      }/${currentImage.public_id}.${currentImage.format}`}
+                      width={navigation ? 1280 : 1920}
+                      height={navigation ? 853 : 1280}
+                      priority
+                      alt="Next.js Conf image"
+                      onLoadingComplete={() => setLoaded(true)}
+                    />
+                  </div>
+                  <div style={showForm ? {} : { display: "none" }}>
+                    <div className="w-full max-w-xs">
+                      <form className="mb-4 rounded bg-white px-8 pb-8 pt-6 shadow-md">
+                        <div className="mb-4">
+                          <label
+                            className="mb-2 block text-sm font-bold text-gray-700"
+                            htmlFor="username"
+                          >
+                            Username
+                          </label>
+                          <input
+                            className="focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none"
+                            id="username"
+                            type="text"
+                            placeholder="Username"
+                          />
+                        </div>
+                        <div className="mb-6">
+                          <label
+                            className="mb-2 block text-sm font-bold text-gray-700"
+                            htmlFor="password"
+                          >
+                            Password
+                          </label>
+                          <input
+                            className="focus:shadow-outline mb-3 w-full appearance-none rounded border border-red-500 px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none"
+                            id="password"
+                            type="password"
+                            placeholder="******************"
+                          />
+                          <p className="text-xs italic text-red-500">
+                            Please choose a password.
+                          </p>
+                        </div>
+                        <div className="flex items-center justify-between">
+                          <button
+                            className="focus:shadow-outline rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700 focus:outline-none"
+                            type="button"
+                          >
+                            Sign In
+                          </button>
+                          <a
+                            className="inline-block align-baseline text-sm font-bold text-blue-500 hover:text-blue-800"
+                            href="#"
+                          >
+                            Forgot Password?
+                          </a>
+                        </div>
+                      </form>
+                      <p className="text-center text-xs text-gray-500">
+                        &copy;2020 Acme Corp. All rights reserved.
+                      </p>
+                    </div>
+                  </div>
+                </div>
               </motion.div>
             </AnimatePresence>
           </div>
@@ -101,7 +162,7 @@ export default function SharedModal({
                   {index > 0 && (
                     <button
                       className="absolute left-3 top-[calc(50%-16px)] rounded-full bg-black/50 p-3 text-white/75 backdrop-blur-lg transition hover:bg-black/75 hover:text-white focus:outline-none"
-                      style={{ transform: 'translate3d(0, 0, 0)' }}
+                      style={{ transform: "translate3d(0, 0, 0)" }}
                       onClick={() => changePhotoId(index - 1)}
                     >
                       <ChevronLeftIcon className="h-6 w-6" />
@@ -110,7 +171,7 @@ export default function SharedModal({
                   {index + 1 < images.length && (
                     <button
                       className="absolute right-3 top-[calc(50%-16px)] rounded-full bg-black/50 p-3 text-white/75 backdrop-blur-lg transition hover:bg-black/75 hover:text-white focus:outline-none"
-                      style={{ transform: 'translate3d(0, 0, 0)' }}
+                      style={{ transform: "translate3d(0, 0, 0)" }}
                       onClick={() => changePhotoId(index + 1)}
                     >
                       <ChevronRightIcon className="h-6 w-6" />
@@ -118,7 +179,7 @@ export default function SharedModal({
                   )}
                 </>
               )}
-              <div className="absolute top-0 right-0 flex items-center gap-2 p-3 text-white">
+              <div className="absolute right-0 top-0 flex items-center gap-2 p-3 text-white">
                 {navigation ? (
                   <a
                     href={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/${currentImage.public_id}.${currentImage.format}`}
@@ -152,8 +213,15 @@ export default function SharedModal({
                 >
                   <ArrowDownTrayIcon className="h-5 w-5" />
                 </button>
+                <button
+                  onClick={toggleShowForm}
+                  className="rounded-full bg-black/50 p-2 text-white/75 backdrop-blur-lg transition hover:bg-black/75 hover:text-white"
+                  title="Download fullsize version"
+                >
+                  <PencilSquareIcon className="h-5 w-5" />
+                </button>
               </div>
-              <div className="absolute top-0 left-0 flex items-center gap-2 p-3 text-white">
+              <div className="absolute left-0 top-0 flex items-center gap-2 p-3 text-white">
                 <button
                   onClick={() => closeModal()}
                   className="rounded-full bg-black/50 p-2 text-white/75 backdrop-blur-lg transition hover:bg-black/75 hover:text-white"
@@ -172,29 +240,29 @@ export default function SharedModal({
             <div className="fixed inset-x-0 bottom-0 z-40 overflow-hidden bg-gradient-to-b from-black/0 to-black/60">
               <motion.div
                 initial={false}
-                className="mx-auto mt-6 mb-6 flex aspect-[3/2] h-14"
+                className="mx-auto mb-6 mt-6 flex aspect-[3/2] h-14"
               >
                 <AnimatePresence initial={false}>
                   {filteredImages.map(({ public_id, format, id }) => (
                     <motion.button
                       initial={{
-                        width: '0%',
+                        width: "0%",
                         x: `${Math.max((index - 1) * -100, 15 * -100)}%`,
                       }}
                       animate={{
                         scale: id === index ? 1.25 : 1,
-                        width: '100%',
+                        width: "100%",
                         x: `${Math.max(index * -100, 15 * -100)}%`,
                       }}
-                      exit={{ width: '0%' }}
+                      exit={{ width: "0%" }}
                       onClick={() => changePhotoId(id)}
                       key={id}
                       className={`${
                         id === index
-                          ? 'z-20 rounded-md shadow shadow-black/50'
-                          : 'z-10'
-                      } ${id === 0 ? 'rounded-l-md' : ''} ${
-                        id === images.length - 1 ? 'rounded-r-md' : ''
+                          ? "z-20 rounded-md shadow shadow-black/50"
+                          : "z-10"
+                      } ${id === 0 ? "rounded-l-md" : ""} ${
+                        id === images.length - 1 ? "rounded-r-md" : ""
                       } relative inline-block w-full shrink-0 transform-gpu overflow-hidden focus:outline-none`}
                     >
                       <Image
@@ -203,8 +271,8 @@ export default function SharedModal({
                         height={120}
                         className={`${
                           id === index
-                            ? 'brightness-110 hover:brightness-110'
-                            : 'brightness-50 contrast-125 hover:brightness-75'
+                            ? "brightness-110 hover:brightness-110"
+                            : "brightness-50 contrast-125 hover:brightness-75"
                         } h-full transform object-cover transition`}
                         src={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/c_scale,w_180/${public_id}.${format}`}
                       />
@@ -217,5 +285,5 @@ export default function SharedModal({
         </div>
       </div>
     </MotionConfig>
-  )
+  );
 }
